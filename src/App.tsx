059f9e27b432d701f950b34488954c847ddf8957
llOrTeGall <IvanOrtega_97@hotmail.com>
2023-06-16 17:44:08 -0500

    agregando main

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,12 @@
+import { Nav } from "./components/Nav";
+import { Main } from "./components/Main";
+
 function App() {
 
   return (
     <>
-      <nav className='flex justify-evenly w-full h-auto py-2 bg-emerald-300'>
-        <figure className='flexp'>
-          <img width={50} src="../src/assets/logo.png" alt="foto" />
-        </figure>
-        <ul className='flex'>
-          <li className='m-auto mx-6'><a href="#">Home</a></li>
-          <li className='m-auto mx-6'><a href="#">Contac</a></li>
-          <li className='m-auto mx-6'><a href="#">About</a></li>
-          <li className='m-auto mx-6'><a href="#">Products</a></li>
-          <li className='m-auto mx-6'><a href="#">Log In</a></li>
-        </ul>
-      </nav >
+      <Nav />
+      <Main />
     </>
   )
 }
---
+++ src/components/Main.tsx
@@ -0,0 +1,10 @@
+export const Main = () => {
+  return (
+    <main>
+      <section className='Hero'>
+        <h3>This Is My Page</h3>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Impedit soluta distinctio nostrum, molestias pariatur quae cupiditate ab, veniam neque aperiam explicabo vitae sequi omnis laboriosam eaque ipsa porro quod corporis.</p>
+      </section>
+    </main>
+  )
+}
