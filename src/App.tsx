18aff05a5542bfd5b02480c83a9ad5b3504fa221
llOrTeGall <IvanOrtega_97@hotmail.com>
2023-06-16 17:39:32 -0500

    agregando la nav

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,18 @@ function App() {
 
   return (
     <>
-      <div className='bg-red-400'>Hello World</div>
+      <nav className='flex justify-evenly w-full h-auto py-2 bg-emerald-300'>
+        <figure className='flexp'>
+          <img width={50} src="../src/assets/logo.png" alt="foto" />
+        </figure>
+        <ul className='flex'>
+          <li className='m-auto mx-6'><a href="#">Home</a></li>
+          <li className='m-auto mx-6'><a href="#">Contac</a></li>
+          <li className='m-auto mx-6'><a href="#">About</a></li>
+          <li className='m-auto mx-6'><a href="#">Products</a></li>
+          <li className='m-auto mx-6'><a href="#">Log In</a></li>
+        </ul>
+      </nav >
     </>
   )
 }
