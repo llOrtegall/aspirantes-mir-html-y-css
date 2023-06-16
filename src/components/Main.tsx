ba418f423f0e6a168d22a3da22244c347757fc3f
llOrTeGall <IvanOrtega_97@hotmail.com>
2023-06-16 17:52:20 -0500

    agregando semidiseño

--- src/components/Main.tsx
+++ src/components/Main.tsx
@@ -4,8 +4,8 @@ export const Main = () => {
   return (
     <main>
       <section className='Hero'>
-        <h3>This Is My Page</h3>
-        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Impedit soluta distinctio nostrum, molestias pariatur quae cupiditate ab, veniam neque aperiam explicabo vitae sequi omnis laboriosam eaque ipsa porro quod corporis.</p>
+        <h3 className='text-2xl font-bold my-2 '>This Is My Page</h3>
+        <p className='w-4/6 pb-4 text-center'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Impedit soluta distinctio nostrum, molestias pariatur quae cupiditate ab, veniam neque aperiam explicabo vitae sequi omnis laboriosam eaque ipsa porro quod corporis. Lorem ipsum dolor sit amet consectetur adipisicing elit. Ea corrupti sint, corporis, excepturi provident qui sunt quod quas nemo mollitia eum? Quaerat autem eaque voluptate ipsum tempora similique reiciendis magnam. Lorem ipsum dolor sit amet consectetur adipisicing elit. Ea consequuntur doloribus optio nemo neque! Natus illum omnis fugiat ea, ipsum autem ducimus dolorum suscipit, deserunt asperiores voluptate voluptatum beatae tenetur.</p>
       </section>
     </main>
   )
