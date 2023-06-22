01214df223a428b8238272338202d0fa844712a3
llOrtegall <IvanOrtega_97@hotmail.com>
2023-06-21 20:23:52 -0500

    instala eslint y todo css

--- README.md
+++
@@ -1,3 +0,0 @@
-# React_App_Learning_05
-
-My Proyecto De React TS + SWC
--- src/components/Footer.tsx
+++
@@ -1,7 +0,0 @@
-export const Footer = () => {
-    return (
-        <footer>
-            <span> @llOrTeGall - MIT  2023</span>
-        </footer>
-    )
-}
