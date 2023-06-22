64862da4e038835b410792d0f67f6f0341ffa0ce
llOrtegall <IvanOrtega_97@hotmail.com>
2023-06-21 21:20:22 -0500

    continuar con el proyect

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,26 @@
+import { useState } from "react"
+
+const mockTodo = [
+  {
+    id: '1',
+    title: 'todo 1',
+    completed: false
+  },
+  {
+    id: '2',
+    title: 'todo 2',
+    completed: false
+  },
+  {
+    id: '3',
+    title: 'todo 3',
+    completed: false
+  }
+]
+
 const App = (): JSX.Element => {
+  const [todo, setTodo] = useState(mockTodo)
+
   return (
     <>
       <h1>Hello World!</h1>
